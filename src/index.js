ac3ee70b448ef2e1592cabd1af46e6d232e20602
kmsedu <kmsedu@tuta.io>
2022-04-17 00:19:36 +1000

    Initial commit, directory structure, mockup HTML and layout CSS complete.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./reset.css"
+import "./normalize.css"
+import "./style.css"
+import githubIcon from "./GitHub-Mark-Light-32px.png";
+import menuIcon1 from "./menu-item-1.jpg";
+
+console.log("Webpack is running.");
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
